9970dd977666ea219c2d1bb3ab8d36d468009e80
Remy Rojas <remy.rojas@protonmail.com>
2016-12-07 18:10:26 +0100

    #NotMyGame

--- src/main/java/com/rojas/remy/game/model/Config.java
+++ src/main/java/com/rojas/remy/game/model/Config.java
@@ -10,11 +10,11 @@ import com.rojas.remy.game.helper.TwoDimPosition;
 public class Config {
 
     //images
-    public static String ENEMY_IMAGE1 = "/images/enemy1.png";
+    public static String ENEMY_IMAGE1 = "/images/rsz_1rsz_hillary1.png";
     public static String ENEMY_IMAGE2 = "/images/enemy2.png";
-    public static String CANNON_IMAGE = "/images/cannon.png";
-    public static String MISSILE_IMAGE = "/images/missile.png";
-    public static String COLLISION_IMAGE = "/images/collision.png";
+    public static String CANNON_IMAGE = "/images/rsz_1rsz_trump.png";
+    public static String MISSILE_IMAGE = "/images/rsz_1rsz_twitter.png";
+    public static String COLLISION_IMAGE = "/images/rsz_1rsz_hillary2.png";
 
     public static APosition CANNON_INIT_POS = new TwoDimPosition(40,40);
 
